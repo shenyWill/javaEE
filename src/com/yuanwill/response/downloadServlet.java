ee7bc400a4732d01d477830132389baff0e4a592
shenyuan <775486362@qq.com>
2018-11-27 15:43:28 +0800

    complete response

--- src/com/yuanwill/response/downloadServlet.java
+++ src/com/yuanwill/response/downloadServlet.java
@@ -3,6 +3,7 @@ package com.yuanwill.response;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import sun.misc.BASE64Encoder;
 
 /**
  * Servlet implementation class downloadServlet
@@ -33,11 +36,31 @@ public class downloadServlet extends HttpServlet {
 		
 		// 解决中文文件名乱码报错
 		filename = new String(filename.getBytes("ISO8859-1"),"utf-8");
+		
+		// 获得请求头中的user-agent
+		String agent = request.getHeader("User-Agent");
+		
+		//根据不同浏览器进行不同的编码
+		String filenameEncoder = "";
+		if (agent.contains("MSIE")) {
+			// IE浏览器
+			filenameEncoder = URLEncoder.encode(filename, "utf-8");
+			filenameEncoder = filenameEncoder.replace("+", " ");
+		} else if (agent.contains("Firefox")) {
+			// 火狐浏览器
+			BASE64Encoder base64Encoder = new BASE64Encoder();
+			filenameEncoder = "=?utf-8?B?"
+					+ base64Encoder.encode(filename.getBytes("utf-8")) + "?=";
+		} else {
+			// 其它浏览器
+			filenameEncoder = URLEncoder.encode(filename, "utf-8");				
+		}
+		
 		// 要下载的文件类型
 		response.setContentType(this.getServletContext().getMimeType(filename));
 		
 		// 告诉服务器不解析，直接下载
-		response.setHeader("Content-Disposition", "attachment;filename=" + filename);
+		response.setHeader("Content-Disposition", "attachment;filename=" + filenameEncoder);
 		
 		String realpath = getServletContext().getRealPath("/download/" + filename);
 		// 获得该文件的输入流

c27b23dda66c5755ab98d17e537c3aab256d9947
shenyuan <775486362@qq.com>
2018-11-29 10:49:21 +0800

    register module

---
+++ src/com/yuanwill/register/RegisterServlet.java
@@ -0,0 +1,79 @@
+package com.yuanwill.register;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.dbutils.QueryRunner;
+
+import com.yuanwill.utils.JDBCUtils;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 获取数据
+		Map<String, String[]> properties = request.getParameterMap();
+		// 将散装数据存储到javabean中
+		User user = new User();
+		try {
+			BeanUtils.populate(user, properties);
+		} catch (IllegalAccessException e) {
+			
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			
+			e.printStackTrace();
+		}
+		// 手动封装UID
+		user.setUid(UUID.randomUUID().toString());
+		System.out.println(user);
+		// 存储数据
+		try {
+			register(user);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void register(User user) throws SQLException{
+		QueryRunner qr = new QueryRunner(JDBCUtils.getDataSource());
+		String sql = "insert into user values (?,?,?,?,?,?,?,?,?,?)";
+		qr.update(sql, user.getUid(),user.getUsername(),user.getPassword(),user.getName(),
+				user.getEmail(),user.getTelephone(),user.getBirthday(),user.getSex(),user.getState(),user.getCode());
+	}
+
+}
